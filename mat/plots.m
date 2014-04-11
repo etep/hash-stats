03b26b1b61f5e693c17ce7bdedf756785e443919
Pete Stevenson <etep.nosnevets@gmail.com>
2014-04-10 23:24:37 -0700

    plots.m

--- mat/plots.m
+++ mat/plots.m
@@ -5,11 +5,11 @@ m  = 1024;
 n  = 48;
 l  = 1.0;
 
-minways = 40;
-maxways = 60;
+minways = 1;
+maxways = 100;
 minload = 0.8;
 maxload = 1.3;
-vn = minways:10:maxways;
+vn = minways:1:maxways;
 vl = minload:0.0025:maxload;
 
 params.fs = 16;       % font size
@@ -19,6 +19,6 @@ params.resolution = '-r400';
 
 % 1st figure: overflow versus number of ways
 % 2nd figure: overflow versus table load
-%vsways( m, vn, l, params );
+vsways( m, vn, l, params );
 vsload( m, n, vl, params );
 
